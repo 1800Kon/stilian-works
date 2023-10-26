1a83ce68514899e82173242e8c66ec0636c26837
Konstantin <konstantinbogev@gmail.com>
2023-10-26 19:20:26 +0200

    some work

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,30 +1,31 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { AcmeLogo } from "../AcmeLogo.jsx"
 export default function NavigationBar() {
     return (
         <Navbar className="bg-orange-100">
-            <NavbarBrand>
-                <NavbarContent>
+            <NavbarBrand className="">
+                < AcmeLogo />
+                <p>
                     Stilian Works
-                </NavbarContent>
-                <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Philosophy
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem isActive>
-                        <Link href="#" aria-current="page">
-                            Collections
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Contact
-                        </Link>
-                    </NavbarItem>
-                </NavbarContent>
-            </NavbarBrand>
-
-        </Navbar>
+                </p>
+            </NavbarBrand >
+            <NavbarContent className="gap-8" justify="end">
+                <NavbarItem>
+                    <Link color="foreground" href="/Philosophy">
+                        Philosophy
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page">
+                        Collections
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Contact
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar >
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
 "use client"
 import { Listbox, ListboxItem } from "@nextui-org/react";
-import { ListboxWrapper } from "./components/ListboxWrapper";
+import { Image } from "@nextui-org/react";
 
 export default function Home() {
     return (
-        <div className="grid grid-cols-3 bg-orange-200">
+        <div className="grid grid-cols-3">
             {/* Left side */}
-            <div className="bg-blue-100 col-span-1">
-                <ListboxWrapper>
-                    <Listbox
-                        aria-label="Actions">
-                        <ListboxItem key="new">Verev</ListboxItem>
-                        <ListboxItem key="copy">Test 1</ListboxItem>
-                        <ListboxItem key="edit">Test 2</ListboxItem>
-                        <ListboxItem key="edit">Test 3</ListboxItem>
-
+            <div className="flex justify-center items-center col-span-1 bg-blue-800">
+                <div className="text-center grow">
+                    <Listbox className=" gap-8">
+                        <ListboxItem key="new"><p className="text-4xl text-slate-100">Verev</p></ListboxItem>
+                        <ListboxItem key="copy"><p className="text-4xl text-slate-100">Indignus</p></ListboxItem>
+                        <ListboxItem key="edit"><p className="text-4xl text-slate-100">Something</p></ListboxItem>
+                        <ListboxItem key="edit"><p className="text-4xl text-slate-100">Fortnite</p></ListboxItem>
                     </Listbox>
-                </ListboxWrapper>
+                </div>
             </div>
             {/* Right side */}
-            <div className="bg-yellow-100 col-span-2">
-
+            <div className="flex justify-center items-center col-span-2 bg-blue-400">
+                <Image
+                    alt="NextUI hero Image"
+                    src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                />
             </div>
         </div >
     )
